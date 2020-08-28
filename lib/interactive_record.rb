d790bf904cbf44226d8c9dee1844ee6f87d3d1ce
Megan (AKA Pilahi) Moran <kamokumoran@gmail.com>
2020-08-28 19:18:41 +0000

    Automatically backed up by Learn

--- lib/interactive_record.rb
+++ lib/interactive_record.rb
@@ -1,6 +1,24 @@
 require_relative "../config/environment.rb"
 require 'active_support/inflector'
+require 'pry'
 
 class InteractiveRecord
   
+  def self.table_name
+  end
+  
+  def self.column_names
+  end 
+  
+  def initialize
+  end 
+  
+  def table_name_for_insert
+  end 
+  
+  def cool_names_for_insert
+  end 
+  
+  def values_for_insert
+  end
 end
