875272638afad042802033f96784b682203837b2
Megan (AKA Pilahi) Moran <kamokumoran@gmail.com>
2020-08-28 19:26:28 +0000

    Automatically backed up by Learn

--- lib/interactive_record.rb
+++ lib/interactive_record.rb
@@ -10,7 +10,8 @@ class InteractiveRecord
   end  
   
   def save
-    DB[:conn].execute("INSERT INTO #{table_name_for_insert} (#{col_names_for_insert}) VALUES (#{values_for_insert})")
+    sql = "INSERT INTO #{table_name_for_insert} (#{col_names_for_insert}) VALUES (#{values_for_insert})")
+    DB[:conn].execute(sql)
     @id = DB[:conn].execute("SELECT last_insert_rowid() FROM #{table_name_for_insert}")[0][0]
   end
   
@@ -20,8 +21,7 @@ class InteractiveRecord
   
   def self.column_names
   end 
-  
-  
+ 
   
   def table_name_for_insert
   end 
